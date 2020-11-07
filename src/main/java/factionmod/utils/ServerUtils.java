7edf675f441c51d7fe3874e6b91d422236f3451b
LubieKakao1212 <grzesio.go@gmail.com>
2020-11-07 15:01:15 +0100

    init and player finder

---
+++ src/main/java/factionmod/utils/ServerUtils.java
@@ -0,0 +1,63 @@
+package factionmod.utils;
+
+import java.util.List;
+import java.util.UUID;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.profiler.Profiler;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerList;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
+/**
+ * Contains usefull methods relatives to the {@link MinecraftServer}.
+ *
+ * @author BrokenSwing
+ *
+ */
+public class ServerUtils {
+
+    private static MinecraftServer server;
+    private static Profiler        profiler;
+
+    public static void init() {
+        server = FMLCommonHandler.instance().getMinecraftServerInstance();
+        profiler = server.profiler;
+    }
+
+    /**
+     * Returns the instance of the Minecraft server.
+     * 
+     * @return the instance of the server
+     */
+    public static MinecraftServer getServer() {
+        return server;
+    }
+
+    /**
+     * Returns the player with the given {@link UUID}
+     * 
+     * @param uuid
+     *            The UUID
+     * @return the player
+     */
+    public static EntityPlayerMP getPlayer(final UUID uuid) {
+        return getServer().getPlayerList().getPlayerByUUID(uuid);
+    }
+
+    public static List<EntityPlayerMP> getPlayers()
+    {
+        return getServer().getPlayerList().getPlayers();
+    }
+
+
+    /**
+     * Returns the profiler of the server.
+     * 
+     * @return the profiler
+     */
+    public static Profiler getProfiler() {
+        return profiler;
+    }
+
+}
